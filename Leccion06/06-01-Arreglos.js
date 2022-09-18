0d828da2a38af438502ba8d5fd259e6985b5074b
Samantha González <samujm76@gmail.com>
2022-09-18 14:24:45 -0500

    Crear un Arreglo

---
+++ Leccion06/06-01-Arreglos.js
@@ -0,0 +1,13 @@
+/*--------------------------------------------------------------《 ARREGLO 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Tipo object que permite almacenar varios valores -------------------*/
+
+//Como declarar un arreglo
+
+/*-------------------------- ▀ Forma antigua de declarar -------------------*/
+// let autos = new Array('BMW', 'Mercedes Benz', 'Volvo');
+
+/*-------------------------- ▀ Si ya no se declara algun otro tipo de referencia se puede declarar la palabra const -------------------*/
+/*-------------------------- ▀ Por referencia es el espacio de memoria que se asigna para ese arreglo -------------------*/
+/*-------------------------- ▀ La variable autos almacena la referencia donde se encuentra en memoria el arreglo que se va a crear -------------------*/
+const autos = ['BMW', 'Mercedes Benz', 'Volvo'];
+console.log(autos);
