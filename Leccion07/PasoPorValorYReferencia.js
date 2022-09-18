bde7e285e60e8ca071a3947e7d044e5458b05c63
Samantha González <samujm76@gmail.com>
2022-09-18 18:12:45 -0500

    Paso por Referencia  y Declarar un objeto (basico)

--- Leccion07/PasoPorValorYReferencia.js
+++ Leccion07/PasoPorValorYReferencia.js
@@ -13,4 +13,32 @@ cambiarValor(x);
 /*-------------------------- ▀ La variable x NO sufre ningun cambio por que solo pasó su valor, una copia de su valor a a, entonces existen dos variables x y a -------------------*/
 console.log(x);
 /*-------------------------- ▀ La variable es un argumento de cambiarValor() y se destruye al finalizar -------------------*/
-console.log(a);
+// console.log(a);
+
+
+
+
+
+
+/*--------------------------------------------------------------《 PASO POR REFERENCIA 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ La variable de persona almacena una referencia a un objeto -------------------*/
+//Crear Objeto
+// const persona = new Object();
+
+//Crear Objeto con parametros
+const persona = {
+    nombre : 'Juan',
+    apellido : 'Perez'
+}
+
+/*-------------------------- ▀ El metodo accede a la referencia en memotia del mismo objeto -------------------*/
+/*-------------------------- ▀ persona y p1 acceden a la misma referencia en memoria -------------------*/
+function cambiarValorObjeto(p1){ //Apunta al objeto
+    p1.nombre  = 'Carlos';
+    p1.apellido = 'Lara';
+}
+
+//Paso por referencia
+cambiarValorObjeto(persona); //mandando persona que es la misma referencia en memoria
+console.log( persona );
+console.log(persona.apellido);
