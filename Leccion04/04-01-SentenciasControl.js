016e2b592259b33a3bde25233973db43554b0576
Samantha González <samujm76@gmail.com>
2022-09-17 23:41:24 -0500

    SENTENCIAS CONTROL: if else if

--- Leccion04/04-01-SentenciasControl.js
+++ Leccion04/04-01-SentenciasControl.js
@@ -5,3 +5,19 @@ if ( 2>3 ) {
 } else {
     console.log("Condición falsa"); 
 }
+
+
+
+let numero = -1;
+
+if (numero == 1) {
+    console.log("Número uno");
+} else if( numero == 2) {
+    console.log("Número dos");
+} else if( numero == 3) {
+    console.log("Número tres");
+} else if( numero == 4){
+    console.log("Número cuatro");
+} else {
+    console.log("Número desconocido");
+}
