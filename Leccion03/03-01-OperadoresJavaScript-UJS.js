89306907a05fd0e94d3a1eb0edbd44dcf2b33df6
Samantha González <samujm76@gmail.com>
2022-09-17 21:57:54 -0500

    incremento y decremento (++ y --)

--- Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -17,3 +17,29 @@ console.log("Resultado de operación módulo (residuo): " + z);
 
 z = a ** b;
 console.log("Resultado de operador exponente: " + z);
+
+/*--------------------------------------------------------------《 INCREMENTO 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Pre-incremento (el operador ++ antes de la variable) -------------------*/
+/*-------------------------- ▀ Se incrementa primero y despues se asigna -------------------*/
+z = ++a;
+console.log(a);
+console.log(z);
+
+/*-------------------------- ▀ Post-incremento (el operador ++ despues de la variable) -------------------*/
+/*-------------------------- ▀ Primero se utiliza el valor original de la variable y despues se incrementa -------------------*/
+z = b++;
+console.log(b);
+console.log(z);
+
+/*--------------------------------------------------------------《 DECREMENTO 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Pre-decremento (el operador ++ antes de la variable) -------------------*/
+/*-------------------------- ▀ Se decrementa primero y despues se asigna -------------------*/
+z = --a;
+console.log(a);
+console.log(z);
+
+/*-------------------------- ▀ Post-decremento (el operador ++ despues de la variable) -------------------*/
+/*-------------------------- ▀ Primero se utiliza el valor original de la variable y despues se decrementa -------------------*/
+z = b--;
+console.log(b);
+console.log(z);
