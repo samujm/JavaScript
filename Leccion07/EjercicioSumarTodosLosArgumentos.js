026d8445b849dc63cc85be0de80263d49456ae11
Samantha González <samujm76@gmail.com>
2022-09-18 17:33:18 -0500

    Ejercico Sumar Todos Los Argumentos (cambio)

--- Leccion07/EjercicioSumarTodosLosArgumentos.js
+++ Leccion07/EjercicioSumarTodosLosArgumentos.js
@@ -1,4 +1,5 @@
-let resultado = sumarTodo(5, 4, 13, 10, 9);
+let resultado = sumarTodo(5, 4, 13, 10, 9, 10, 11, 3);
+console.log(resultado);
 
 function sumarTodo(){
     let suma = 0;
@@ -6,5 +7,5 @@ function sumarTodo(){
     for (let i = 0; i < arguments.length; i++) {
         suma += arguments[i];
     }
-    console.log(suma);
+    return suma;
 }
