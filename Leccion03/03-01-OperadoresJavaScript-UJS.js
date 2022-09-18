8a6fd5269e0b6ce6297ad42f4dc8c0bc2556c947
Samantha González <samujm76@gmail.com>
2022-09-17 21:49:24 -0500

    Operadores (sum, rest, multi, div, residuo, expo)

---
+++ Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,19 @@
+let a = 3, b = 2;
+
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+z = a * b;
+console.log("Resultado de la multiplicación: " + z);
+
+z = a / b;
+console.log("Resultado de la división: " + z);
+
+z = a % b;
+console.log("Resultado de operación módulo (residuo): " + z);
+
+z = a ** b;
+console.log("Resultado de operador exponente: " + z);
