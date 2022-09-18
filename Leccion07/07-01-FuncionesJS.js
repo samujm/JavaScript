990bb0719a71d7fd700b400300888148787df71f
Samantha González <samujm76@gmail.com>
2022-09-18 15:03:50 -0500

    Return en una Funcion

--- Leccion07/07-01-FuncionesJS.js
+++ Leccion07/07-01-FuncionesJS.js
@@ -2,9 +2,9 @@
 /*-------------------------- ▀ Una función es un codigo reutilizable -------------------*/
 /*-------------------------- ▀ Se puede mandar llamar tantas veces como se necesite -------------------*/
 
-
+//HOISTING
 /*-------------------------- ▀ No importa donde se mande llamar mientras se encuentre definido,a esto se le llama HOISTING -------------------*/
-miFuncion(4,2);
+// miFuncion(4,2);
 
 
 //Declarar la funcion
@@ -12,8 +12,14 @@ miFuncion(4,2);
 /*-------------------------- ▀ En los parentesis van argumentos de la funcion, es la información de entrada con que puede trabajar la funcion -------------------*/
 /*-------------------------- ▀ puede tener o no argumentos -------------------*/
 function miFuncion(a, b) {
-    console.log("Suma: " + (a + b));
+    // console.log("Suma: " + (a + b));
+    /*-------------------------- ▀ Retornar el resultado, para ello la funcion se asigna a una variable -------------------*/
+    return a + b;
 }
 
 //Llamada a la funcion
 miFuncion(2,3);
+
+/*-------------------------- ▀ Asignar el return de la suma a una variable para poder mostrar el resultado -------------------*/
+let resultado = miFuncion(2,3);
+console.log(resultado);
