77f5a9decb98ee87a1e39cd37fee621a617652e5
Samantha González <samujm76@gmail.com>
2022-09-17 23:12:02 -0500

    Operador ternario

---
+++ Leccion03/OperadorTernario.js
@@ -0,0 +1,10 @@
+/*--------------------------------------------------------------《 OPERADOR TERNARIO 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Igual a if else -------------------*/
+/*-------------------------- ▀ Condition ? Expr1(verdadero) : Expr2(falso) -------------------*/
+/*-------------------------- ▀ Se pueden regresar diferentes valores -------------------*/
+let resultado = (1>2) ? "Verdadero" : "Falso";
+console.log(resultado);
+
+let numero = 110;
+resultado = (numero % 2 == 0 ) ? "Número par" : "Número impar";
+console.log(resultado);
