808f0ebd7f24c30a02faefc3066d847ac574b7e0
Samantha González <samujm76@gmail.com>
2022-09-17 22:09:09 -0500

    Precedencia de operadores

---
+++ Leccion03/PrecedenciaDeOperadores.js
@@ -0,0 +1,18 @@
+let a = 3, b = 2, c = 1, d= 4;
+
+let z = a * b + c / d;
+console.log(z);
+
+z = c + a * b / d;
+console.log(z);
+
+z = (c + a) * b / c;
+console.log(z);
+
+// 1.- Parentesis () []
+// 2.- Unarios (incremento ++, decremento --)
+// 3.- * / %
+// 4.- + -
+// 5.- >> <<
+// 6.- < <= > >= 
+// 7.- == != === !==
