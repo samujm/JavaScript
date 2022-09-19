3e0cb365cde16108cc424754a76aca28a20f0ce4
Samantha González <samujm76@gmail.com>
2022-09-19 17:07:47 -0500

    Objetos

---
+++ Leccion08/08-01-Objects.js
@@ -0,0 +1,18 @@
+/*--------------------------------------------------------------《 OBJETOS 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Contiene atributos y metodos -------------------*/
+
+//DEFINIENDO UN OBJETO
+/*-------------------------- ▀ Se crea un objeto en memoria que contiene una referencia -------------------*/
+/*-------------------------- ▀ Se crea un objeto con las propiedades nombre, apellido, email, edad; con una direccion de memoria asignada la variable persona por ej 0x333 -------------------*/
+let persona = {
+    /*-------------------------- ▀ No todos los valores tienen que ser cadena -------------------*/
+    nombre: 'Juan',
+    apellido: 'Perez',
+    email: 'jperez@mail.com',
+    edad: 28
+}
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.apellido);
+console.log(persona);
