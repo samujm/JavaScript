c4e506e9060263ad89047ff38f1a502e7c262a5a
Samantha González <samujm76@gmail.com>
2022-09-19 18:36:03 -0500

    Constructor de un Objeto

--- Leccion08/08-01-Objects.js
+++ Leccion08/08-01-Objects.js
@@ -138,3 +138,24 @@ console.log( persona3.idioma ); //Se almacenó correctamente
 
 
 
+
+
+/*--------------------------------------------------------------《 CONTRUCTOR DE OBJETOS 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Funcion especial para trabajar con objetos en JS -------------------*/
+/*-------------------------- ▀ Constructor empezar con mayuscula -------------------*/
+//Funcion constructor de objetos de tipo Persona
+// function Persona(nombre = 'Juan', apellido, email){ //Se pueden pasar valores aqui
+function Persona(nombre, apellido, email){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+}
+
+//Se crea el objeto Persona
+let padre = new Persona('Juan', 'Perez', 'jperez@mail.com');
+console.log( padre );
+let madre = new Persona('Laura', 'Quintero', 'lquintero@mail.com');
+console.log( madre );
+
+padre.nombre = 'Carlos';
+console.log( padre );
