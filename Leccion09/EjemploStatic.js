48d863e1ad034f7d42beab4ba402ecca24adcb2e
Samantha González <samujm76@gmail.com>
2022-09-20 11:53:41 -0500

    Creación de Constantes Estáticas

--- Leccion09/EjemploStatic.js
+++ Leccion09/EjemploStatic.js
@@ -1,10 +1,20 @@
 class Persona {
     static contadorPersonas = 0;
 
+    /*--------------------------------------------------------------《 ATRIBUTO DE TIPO STATICO QUE FUNCIONA COMO CONSTANTE DE LA CLASE PERSONA 》------------------------------------------------------------------------------------*/
+    /*-------------------------- ▀ Se crea un metodo get static (mayusculas el nombre) y retorna el valor static -------------------*/
+    static get MAX_OBJ(){
+        return 5;
+    }
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
-        this.idPersona = ++Persona.contadorPersonas; //Se asigna a una variable para que sea su id, se incrementa y se asigna
+        if (Persona.contadorPersonas < Persona.MAX_OBJ) {
+            this.idPersona = ++Persona.contadorPersonas; //Se asigna a una variable para que sea su id, se incrementa y se asigna
+        } else {
+            console.log('Se han superado el máximo de objetos permitidos');
+        }
     }
     
     get nombre(){
@@ -71,3 +81,16 @@ let persona2 = new Persona('Karla', 'Ramirez');
 console.log(persona2.toString());
 
 console.log( Persona.contadorPersonas );
+
+/*--------------------------------------------------------------《 ATRIBUTO DE TIPO STATICO QUE FUNCIONA COMO CONSTANTE DE LA CLASE PERSONA 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Se crea un metodo get static (mayusculas el nombre) y retorna el valor static -------------------*/
+console.log( Persona.MAX_OBJ );
+Persona.MAX_OBJ = 10;
+console.log( Persona.MAX_OBJ );
+
+
+let persona3 = new Persona( 'Mariano' , 'Lara' );
+let persona4 = new Persona( 'Armando' , 'Toledo' );
+let persona5 = new Persona( 'Laura' , 'Quintero' );
+console.log( persona5.toString() ); //Ya no se le asigno el ID por que ya es el limite
+console.log( persona4.toString() );
