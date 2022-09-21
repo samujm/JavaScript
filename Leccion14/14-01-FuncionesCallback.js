1640d2d4e46764d22efdc6eedba70172a56c3c79
Samantha González <samujm76@gmail.com>
2022-09-21 13:25:36 -0500

    Funcion Callback

---
+++ Leccion14/14-01-FuncionesCallback.js
@@ -0,0 +1,33 @@
+
+miFuncion2();
+miFuncion1();
+
+
+/*--------------------------------------------------------------《 FUNCIONES NORMALES 》------------------------------------------------------------------------------------*/
+
+function miFuncion1(){
+    console.log('Función 1');
+}
+
+function miFuncion2(){
+    console.log('Función 2');
+}
+
+/*--------------------------------------------------------------《 FUNCION CALLBACK 》------------------------------------------------------------------------------------*/
+/*-------------------------- ▀ Dentro de una funcion se manda llamar otra funcion -------------------*/
+
+// let imp = function imprimir(mensaje){  //Otra forma de aplicar, se asigna la referencia a la variable imp y esa se manda en el argumento de suma
+//     console.log(mensaje);
+// }
+
+function imprimir(mensaje){
+    console.log(mensaje);
+}
+
+function sumar(op1, op2, funcionCallBack){ //funcionCallBack recibe como argumento la referencia de la funcion de imprimir, puede ser el mismo nombre de la funcion
+    let res = op1 + op2;
+    funcionCallBack(`Resultado: ${res}`);
+}
+
+sumar(5, 3, imprimir); //Envia la funcion pero sin ()
+// sumar(5, 3, imp);
