9e757c77955d4c32fbca6d63854b5fda99a33eaf
Samantha González <samujm76@gmail.com>
2022-09-21 17:01:19 -0500

    Promesas, Await, async y setTimeout

--- Leccion15/15-01-Promesas.js
+++ Leccion15/15-01-Promesas.js
@@ -69,4 +69,21 @@ async function funcionConPromesaYAwait(){
     console.log(await miPromesa);
 }
 
-funcionConPromesaYAwait();
+// funcionConPromesaYAwait();
+
+/*--------------------------------------------------------------《 Promesas, Await, async y setTimeout 》-----------------------------------------------------------------------------------*/
+
+//Promesas, Await, async y setTimeout
+
+async function funcionConPromesaAwaitTimeout(){
+    console.log('Inicio función');
+    let miPromesa = new Promise((resolver) => {
+        setTimeout(()=>{
+            resolver('Promesa con await y timeout')
+        },3000);
+    });
+    console.log( await miPromesa );
+    console.log('Fin función');
+}
+
+funcionConPromesaAwaitTimeout();
