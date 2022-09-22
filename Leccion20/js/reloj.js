4ef3ecc62d9e0eddac906091cc90bf9faf6b6680
Samantha González <samujm76@gmail.com>
2022-09-22 18:06:57 -0500

    Ejercicio: Reloj Digital

---
+++ Leccion20/js/reloj.js
@@ -0,0 +1,30 @@
+const  mostrarReloj = () => {
+    let fecha = new Date();
+    let hora = formatoHora(fecha.getHours());
+    let minutos = formatoHora(fecha.getMinutes());
+    let segundos = formatoHora(fecha.getSeconds());
+
+    document.getElementById('hora').innerHTML = `${hora}:${minutos}:${segundos}`;
+
+    const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    const dias = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
+
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDay();
+    let mes = meses[fecha.getMonth()];
+    let año = fecha.getFullYear();
+    let fechaTexto = `${diaSemana}, ${dia} ${mes} ${año}`
+
+    document.getElementById('fecha').innerHTML = fechaTexto;
+
+    document.getElementById('contenedor').classList.toggle('animar');
+}
+
+const formatoHora = (hora) => {
+    if(hora < 10){
+        hora = '0' + hora;
+    }
+    return hora;
+}
+
+setInterval(mostrarReloj, 1000);
